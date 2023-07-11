056be11589de635de437389bc9e582f4dece86b0
_baget <96415977+baget-underscore@users.noreply.github.com>
2023-07-11 20:14:04 +0000

    Website layout 1.0, initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SignMeHub
+========
+A website created to assist you with activities that require a lot of people to sign up.
+========
+Currently in development, please hold off on reporting bugs or other issues.
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function allowDrop(ev) {
-    ev.preventDefault();
-  }
+  ev.preventDefault();
+}
   
-  function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
-  
-  function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
