7ee8843574f16b0c30437d0512757e2c088fbf0b
_baget <96415977+baget-underscore@users.noreply.github.com>
2023-09-19 12:19:41 +0000

    Racisme

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 function allowDrop(ev) {
-  ev.preventDefault();
+    ev.preventDefault();
 }
   
 function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.setData("text", ev.target.id);
 }
 
 function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  $(ev.target).closest('.ws-slot').replaceWith($(document.getElementById(data)));
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData("text");
+    $(ev.target).closest('.ws-slot')
+        .replaceWith($(document.getElementById(data)));
+}
+
+function resetWs(ev) {
+    const ws = $(ev.target.parentElement);
+    $(ev.target.parentElement).replaceWith(
+        `<div class="w3-col l2 m4 s4 ws-slot" ondrop="drop(event)" ondragover="allowDrop(event)">
+        <h4 class="w3-center">Sleep workshop hier</h4>
+        </div>`
+    );
+    ws.appendTo('.ws-itemrow');
 }
