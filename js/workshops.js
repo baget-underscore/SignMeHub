96134fe50955f107d60441031dbbd6480978adf0
_baget <96415977+baget-underscore@users.noreply.github.com>
2023-09-22 09:01:47 +0000

    Tried some things, didnt work.

---
+++ js/workshops.js
@@ -0,0 +1,34 @@
+module.exports = {
+    workshops: [
+        {
+            "name": "Workshop titel",
+            "description": "Dit is de beschrijving van de workshop! Je kunt er doorheen scrollen als er 4 of meer regels zijn. Er zou een uitleg van de workshop in moeten staan, wat je mee moet nemen, etc. De locatie en de gevers van de workshop staan ergens anders.",
+            "location": "Aula"
+        },
+        {
+            "name": "Pizza bakken",
+            "description": "In deze workshop gaan jullie een pizzabodem versieren met allerlei lekkere ingredienten. Daarna moet de pizza nog even in de over en je mag 'm zelf opeten! Er wordt wel een bijdrage van 5 euro verwacht.",
+            "location": "Zorg keuken"
+        },
+        {
+            "name": "Leeg uur",
+            "description": "Voor dit uur heb je nog niks gekozen.",
+            "location": "-"
+        },
+        {
+            "name": "Jacht op de PZ-Schat",
+            "description": "Meldt je bij de receptie voor alle Info die je nodig hebt.",
+            "location": "Receptie"
+        },
+        {
+            "name": "Leeg uur",
+            "description": "Voor dit uur heb je nog niks gekozen.",
+            "location": "-"
+        },
+        {
+            "name": "Leeg uur",
+            "description": "Voor dit uur heb je nog niks gekozen.",
+            "location": "-"
+        }
+    ]
+}
