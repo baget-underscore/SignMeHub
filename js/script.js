7accdf8bc617422d8ad280dc5e3efe73bab55491
_baget <96415977+baget-underscore@users.noreply.github.com>
2023-09-25 13:09:16 +0000

    yes

--- js/script.js
+++ js/script.js
@@ -1,44 +1,55 @@
+let app = {
+    workshops: {},
+    currentDrag: -1,
+};
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
   
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
+    app.currentDrag = ev.target.id;
 }
 
 function drop(ev) {
     ev.preventDefault();
     const data = ev.dataTransfer.getData("text");
     $slot = $(ev.target).closest('.ws-slot');
-    console.log($slot);
-    if ($slot) {
-        console.log('yes');
+    if ($slot.length > 0) {
         $slot.replaceWith($(document.getElementById(data)));
+        app.currentDrag = -1;
+    }
+    $dragarea = $(ev.target).closest('.ws-dragarea');
+    if ($dragarea.length == 1) {
+        resetWs($(document.getElementById('ws_'+app.currentDrag)));
     }
 }
 
-function resetWs(ev) {
-    const ws = $(ev.target.parentElement);
-    $(ev.target.parentElement).replaceWith(
+function resetWs(target) {
+    const $ws = $(target);
+    $ws.replaceWith(
         `<div class="w3-col l2 m4 s4 ws-slot" ondrop="drop(event)" ondragover="allowDrop(event)">
-        <h4 class="w3-center">Sleep workshop hier</h4>
+            <h4 class="w3-center">Sleep workshop hier</h4>
         </div>`
     );
-    ws.appendTo('.ws-itemrow');
+    $ws.appendTo('.ws-dragarea');
 }
 
 $(document).ready( () => {
     $.getJSON("../js/workshops.json", data => {
-        for (const ws of data.workshops) {
+        app.workshops = data.workshops;
+        $.each(app.workshops, function(key, value) {
             $('.ws-itemrow').append(`
-            <div class="w3-col l2 m4 s4 ws-item" draggable="true" ondragstart="drag(event)" id="${data.workshops.indexOf(ws)}">
-                <button class="fa fa-close" onclick="resetWs(event)"></button>
-                <h4>${ws.name}</h4>
-                <div class="ws-description"><p>${ws.description}</p></div>
+            <div class="w3-col l2 m4 s4 ws-item" draggable="true" ondragstart="drag(event)" id="ws_${key}">
+                <button class="fa fa-close" onclick="resetWs(event.target.parentElement)"></button>
+                <h4>${value.name}</h4>
+                <div class="ws-description"><p>${value.description}</p></div>
                 <button style="display:inline;">Info</button>
-                <p class="ws-location">Loc: ${ws.location}</p>
+                <p class="ws-location">Loc: ${value.location}</p>
             </div>
             `);
-        }
-    })
+        });
+    });
 })
+
