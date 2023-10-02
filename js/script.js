7c23e1f7960d52f331e4992f6f898ad5d24d8cde
_baget <96415977+baget-underscore@users.noreply.github.com>
2023-10-02 13:08:31 +0000

    more signup testing

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let app = {
 };
 
 const itemSlotHTML = `
-<div class="w3-col l2 m4 s4 ws-slot" ondrop="drop(event)" ondragover="allowDrop(event)">
+<div class="w3-col l3 m4 s4 ws-slot" ondrop="drop(event)" ondragover="allowDrop(event)">
     <h4 class="w3-center">Sleep workshop hier</h4>
 </div>
 `;
@@ -15,46 +15,51 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
-    console.log(ev);
     ev.dataTransfer.setData("text", ev.target.id);
-    app.currentDrag = ev.target.id;
+    app.currentDrag = ev.target.id
+
+    if (typeof app.currentDrag !== 'string' || !app.currentDrag.startsWith('ws_')) {
+        app.currentDrag = -1;
+    }
 }
 
 function drop(ev) {
     ev.preventDefault();
-    $draggedItem = $(`#${app.currentDrag}`);
-    let dragIndex = $draggedItem.index() + 1;
-    
-    $slot = $(ev.target).closest('.ws-slot');
-    $dragarea = $draggedItem.closest('#intekenRooster');
-    let draggedItemInSlot = $draggedItem.closest('.ws-slot').length == 1;
-    let draggedItemFromSlot = $dragarea.length == 1;
-    if ($slot.length > 0 && !draggedItemInSlot) {
-        if (draggedItemFromSlot) {
-            $(itemSlotHTML).attr('id', dragIndex).insertAfter($draggedItem);
-            for (choice in app.choices) {
-                if (app.choices[choice] == $draggedItem.attr('id') && +choice !== dragIndex) {
-                    delete app.choices[choice];
-                }
+    if (app.currentDrag !== -1) {
+        $draggedItem = $(`#${app.currentDrag}`);
+        $slot = $(ev.target).closest('.ws-slot');
+        let dragIndex = $draggedItem.index() + 1;
+        let targetIndex = $slot.attr('id');
+        
+        $dragarea = $draggedItem.closest('#intekenRooster');
+        let draggedItemInSlot = $draggedItem.closest('.ws-slot').length == 1;
+        let draggedItemFromSlot = $dragarea.length == 1;
+        if ($slot.length > 0 && !draggedItemInSlot) {
+            if (draggedItemFromSlot) {
+                $(itemSlotHTML).attr('id', dragIndex).insertAfter($draggedItem);
+                delete app.choices[dragIndex];
             }
+            $slot.replaceWith($draggedItem);
+            app.choices[targetIndex] = app.currentDrag;
         }
-        $slot.replaceWith($draggedItem);
-        app.choices[$slot.attr('id')] = app.currentDrag;
+        
+        $dragarea = $(ev.target).closest('#intekenOpties');
+        console.log(dragIndex);
+        let draggedItemInDragarea = $draggedItem.closest('#intekenOpties').length == 1;
+        if ($dragarea.length == 1 && !draggedItemInDragarea) {
+            resetWs($draggedItem);
+        }
         app.currentDrag = -1;
-    }
-
-    $dragarea = $(ev.target).closest('#intekenOpties');
-    let draggedItemInDragarea = $draggedItem.closest('#intekenOpties').length == 1;
-    if ($dragarea.length == 1 && !draggedItemInDragarea) {
-        resetWs($draggedItem);
-        app.currentDrag = -1;
+        console.log(app.choices);
     }
 }
-
-function resetWs(target) {
-    const $ws = $(target);
-    $ws.replaceWith(itemSlotHTML);
-    $ws.appendTo('#intekenOpties');
+    
+    function resetWs(target) {
+        const $ws = $(target);
+        let dragIndex = $ws.index() + 1;
+        console.log(dragIndex);
+        $ws.replaceWith($(itemSlotHTML).attr('id', dragIndex));
+        $ws.appendTo('#intekenOpties');
 }
 
 $(document).ready( () => {
@@ -62,7 +67,7 @@ $(document).ready( () => {
         app.workshops = data.workshops;
         $.each(app.workshops, function(key, value) {
             $('#intekenOpties').append(`
-            <div class="w3-col l2 m4 s4 ws-item" draggable="true" ondragstart="drag(event)" id="ws_${key}">
+            <div class="w3-col l3 m4 s2 ws-item" draggable="true" ondragstart="drag(event)" id="ws_${key}">
                 <button class="fa fa-close" onclick="resetWs(event.target.parentElement)"></button>
                 <h4>${value.name}</h4>
                 <div class="ws-description"><p>${value.description}</p></div>
