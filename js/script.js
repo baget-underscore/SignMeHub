bf5d1569f642bf8413eaaacf08abea982a2e36ae
_baget <96415977+baget-underscore@users.noreply.github.com>
2023-10-03 07:20:17 +0000

    fiddled around with responsiveness.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let app = {
 };
 
 const itemSlotHTML = `
-<div class="w3-col l3 m4 s4 ws-slot" ondrop="drop(event)" ondragover="allowDrop(event)">
+<div class="w3-col l3 m4 s6 ws-slot" ondrop="drop(event)" ondragover="allowDrop(event)">
     <h4 class="w3-center">Sleep workshop hier</h4>
 </div>
 `;
@@ -27,11 +27,11 @@ function drop(ev) {
     ev.preventDefault();
     if (app.currentDrag !== -1) {
         $draggedItem = $(`#${app.currentDrag}`);
+        $dragarea = $draggedItem.closest('#intekenRooster');
         $slot = $(ev.target).closest('.ws-slot');
         let dragIndex = $draggedItem.index() + 1;
         let targetIndex = $slot.attr('id');
         
-        $dragarea = $draggedItem.closest('#intekenRooster');
         let draggedItemInSlot = $draggedItem.closest('.ws-slot').length == 1;
         let draggedItemFromSlot = $dragarea.length == 1;
         if ($slot.length > 0 && !draggedItemInSlot) {
@@ -44,22 +44,20 @@ function drop(ev) {
         }
         
         $dragarea = $(ev.target).closest('#intekenOpties');
-        console.log(dragIndex);
         let draggedItemInDragarea = $draggedItem.closest('#intekenOpties').length == 1;
         if ($dragarea.length == 1 && !draggedItemInDragarea) {
             resetWs($draggedItem);
         }
         app.currentDrag = -1;
-        console.log(app.choices);
     }
 }
     
-    function resetWs(target) {
-        const $ws = $(target);
-        let dragIndex = $ws.index() + 1;
-        console.log(dragIndex);
-        $ws.replaceWith($(itemSlotHTML).attr('id', dragIndex));
-        $ws.appendTo('#intekenOpties');
+function resetWs(target) {
+    const $ws = $(target);
+    let dragIndex = $ws.index() + 1;
+    delete app.choices[dragIndex];
+    $ws.replaceWith($(itemSlotHTML).attr('id', dragIndex));
+    $ws.appendTo('#intekenOpties');
 }
 
 $(document).ready( () => {
@@ -67,12 +65,15 @@ $(document).ready( () => {
         app.workshops = data.workshops;
         $.each(app.workshops, function(key, value) {
             $('#intekenOpties').append(`
-            <div class="w3-col l3 m4 s2 ws-item" draggable="true" ondragstart="drag(event)" id="ws_${key}">
+            <div class="w3-col l3 m4 s6 ws-item" draggable="true" ondragstart="drag(event)" id="ws_${key}">
                 <button class="fa fa-close" onclick="resetWs(event.target.parentElement)"></button>
                 <h4>${value.name}</h4>
                 <div class="ws-description"><p>${value.description}</p></div>
-                <button style="display:inline;">Info</button>
-                <p class="ws-location">Loc: ${value.location}</p>
+                
+                <div class="ws-location">
+                    <button>Info</button>
+                    <p>Loc: ${value.location}</p>
+                </div>
             </div>
             `);
         });
